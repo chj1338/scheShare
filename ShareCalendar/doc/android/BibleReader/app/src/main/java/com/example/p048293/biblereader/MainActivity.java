6847737c4446ae0f1e4f6429192d03776544cf2c
P048293 <P048293@SKCC17N00490.SKCC.NET>
2018-03-14 16:25:46 +0900

    android Bible Reader

---
+++ ShareCalendar/doc/android/BibleReader/app/src/main/java/com/example/p048293/biblereader/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.p048293.biblereader;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnGoNewBook(View v) {
+        Intent intent001 = new Intent(this, NewTestamentActivity.class);
+        startActivity(intent001);
+    }
+
+    public void btnGoOldBook(View v) {
+        Intent intent002 = new Intent(this, OldTestamentActivity.class);
+        startActivity(intent002);
+    }
+
+/*
+    // back 버튼을 클릭시 종료 할건지에 대해 묻는다
+    public boolean onBackPressed(int keyCode, KeyEvent event){
+        switch(keyCode){
+            case KeyEvent.KEYCODE_BACK:
+                String alertTitle = getResources().getString(R.string.app_name);
+                String buttonMessage = getResources().getString(R.string.alert_msg_exit);
+                String buttonYes = getResources().getString(R.string.button_yes);
+                String buttonNo = getResources().getString(R.string.button_no);
+
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle(alertTitle)
+                        .setMessage(buttonMessage)
+                        .setPositiveButton(buttonYes, new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // TODO Auto-generated method stub
+                                moveTaskToBack(true);
+                                finish();
+                                android.os.Process.killProcess(android.os.Process.myPid());
+                            }
+                        })
+                        .setNegativeButton(buttonNo, null)
+                        .show();
+        }
+        return true;
+    }
+*/
+
+}
---
+++ ShareCalendar/doc/android/BibleReader/app/src/main/java/com/example/p048293/biblereader/NewTestamentActivity.java
@@ -0,0 +1,121 @@
+package com.example.p048293.biblereader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class NewTestamentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_testament);
+
+        final TextView textView = (TextView)findViewById(R.id.textView001);
+        final Spinner spinner1 = (Spinner)findViewById(R.id.spinner1);    // 책 select
+        final Spinner spinner2 = (Spinner)findViewById(R.id.spinner2);    // 장 select
+
+        // 책 combo item 셋팅
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.bible_new_name_kor, android.R.layout.simple_spinner_item);
+        spinner1.setAdapter(adapter);
+
+        // 장 combo item 셋팅
+        setLastPage(spinner1, spinner2);
+
+        // spinner1 리스너
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // 장 combo item 셋팅
+                setLastPage(spinner1, spinner2);
+
+                readBook(view, spinner1, spinner2, textView);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        // spinner2 리스너
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                readBook(view, spinner1, spinner2, textView);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+
+    // 장 combo item 셋팅
+    public void setLastPage(Spinner spinner1, Spinner spinner2) {
+        // last page item
+        ArrayAdapter adapter_page = ArrayAdapter.createFromResource (this, R.array.bible_new_page, android.R.layout.simple_spinner_item);
+
+        int sp1Position = spinner1.getSelectedItemPosition();
+        int lastPage = Integer.parseInt((String)adapter_page.getItem(sp1Position));
+
+        ArrayList<String> item2 = new ArrayList<>();
+        for(int i=1; i<=lastPage; i++) {
+            item2.add(i+"");
+        }
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, item2);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+    }
+
+
+    // book index 조합으로 찾기
+    public void readBook(View v, Spinner spinner1, Spinner spinner2, TextView textView) {
+        // last page item
+        ArrayAdapter adapter_ac_name = ArrayAdapter.createFromResource (this, R.array.bible_new_name_kor_ac, android.R.layout.simple_spinner_item);
+        int sp1Position = spinner1.getSelectedItemPosition();
+
+        String book = (String)adapter_ac_name.getItem(sp1Position);
+        String index = (String)spinner2.getSelectedItem();
+
+        try {
+            //raw 폴더 읽기
+            InputStream is = getResources().openRawResource(R.raw.bible_new);
+            BufferedReader br2 = new BufferedReader(new InputStreamReader(is));
+
+            String readStr ="";
+            String line = "";
+
+            while((line=br2.readLine())!=null) {
+                String rowData[] = line.split(" ");
+                String colData[] = rowData[0].split(":");
+
+                if(colData[0].equals(book + index)) {
+                    readStr += line + "\n";
+                }
+            }
+
+            textView.setText(readStr);
+            br2.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            textView.setText("파일을 찾을 수 없습니다.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
